01028e0a0b4119a6d2dd99869975ccca49bebca1
Marvin Menzerath <github@marvin-menzerath.de>
2016-03-26 23:52:16 +0100

    Initial upload of all files

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+function updateScreen() {
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+
+	if (h < 10) { h = "0" + h; }
+	if (m < 10) { m = "0" + m; }
+	if (s < 10) { s = "0" + s; }
+
+	document.getElementById("time").innerHTML = (h + " : " + m + " : " + s);
+	document.body.style.background = "#" + h + m + s;
+}
+
+window.onload = setInterval(function () { updateScreen(); }, 100);
